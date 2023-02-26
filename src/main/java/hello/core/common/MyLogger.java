b0fa43120fe8c9b16cd2b3367b5b1c5fa9b05d2d
renechoi <renechoi90@gmail.com>
2023-02-26 22:47:46 +0900

    feat: proxy example

--- src/main/java/hello/core/common/MyLogger.java
+++ src/main/java/hello/core/common/MyLogger.java
@@ -1,6 +1,7 @@
 package hello.core.common;
 
 import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -8,7 +9,8 @@ import javax.annotation.PreDestroy;
 import java.util.UUID;
 
 @Component
-@Scope(value="request")
+//@Scope(value="request")
+@Scope(value="request", proxyMode = ScopedProxyMode.TARGET_CLASS)  // provider를 쓰는 것과 같은 프록시 방식
 public class MyLogger {
 
     private String uuid;
