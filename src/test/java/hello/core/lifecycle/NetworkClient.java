b68ea9db4e35c5b3755eb5542a1288b142c1609d
renechoi <renechoi90@gmail.com>
2023-02-26 10:19:30 +0900

    feat: spring life cycle initializing bean example

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,12 +1,14 @@
 package hello.core.lifecycle;
 
-public class NetworkClient {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean {
     private String url;
 
     public NetworkClient(){
         System.out.println("생성자 호출, url = " + url);
-        connect();
-        call("초기화 연결 메시지");
+
     }
 
     public void setUrl(String url) {
@@ -22,6 +24,20 @@ public class NetworkClient {
     }
 
     public void disconnect(){}
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        // 의존관계 주입이 끝나면 주입해주겠다
+        System.out.println("NetworkClient.afterPropertiesSet");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("NetworkClient.destroy");
+        disconnect();
+    }
 }
 
 
